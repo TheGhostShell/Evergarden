a18ab785e70ba28efd6a7e1f257060b58716362a
John-Weetaker <jjdormevil@gmail.com>
2018-11-20 19:28:34 +0100

    sync laptop to pc

--- src/main/java/com/hanami/cms/CmsApplication.java
+++ src/main/java/com/hanami/cms/CmsApplication.java
@@ -3,11 +3,19 @@ package com.hanami.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.function.Function;
+
 @SpringBootApplication
 public class CmsApplication {
 
 	public static void main(String[] args) {
 		Runnable run = () -> System.out.println("running");
+		
+		Function<String, String> hello = (String name) -> {
+			return "Hello " + name;
+		};
+		
+		System.out.println(hello("john"));
 		
 		(new Thread(run)).start();
 		
