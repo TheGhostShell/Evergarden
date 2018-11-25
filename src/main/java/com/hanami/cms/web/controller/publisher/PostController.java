64c580d9390b934772ed5d3926c289f17d2f0137
john-weetaker <jjdormevil@gmail.com>
2018-11-25 03:43:51 +0100

    wip before merge

--- src/main/java/com/hanami/cms/web/controller/publisher/PostController.java
+++ src/main/java/com/hanami/cms/web/controller/publisher/PostController.java
@@ -1,10 +1,19 @@
 package com.hanami.cms.web.controller.publisher;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="${API_V1}/post")
 public class PostController {
 
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity create() {
+		return new ResponseEntity(HttpStatus.ACCEPTED);
+	}
 }
