dbfca879c203d84e8e2244b9c1482a08591a27b9
john-weetaker <want4planch@gmail.com>
2019-01-17 22:54:47 +0100

    wip time to play dota

--- src/test/java/com/evergarden/cms/context/publisher/infrastructure/controller/PostRouterTest.java
+++ src/test/java/com/evergarden/cms/context/publisher/infrastructure/controller/PostRouterTest.java
@@ -1,7 +1,13 @@
 package com.evergarden.cms.context.publisher.infrastructure.controller;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class PostRouterTest {
 
 }
