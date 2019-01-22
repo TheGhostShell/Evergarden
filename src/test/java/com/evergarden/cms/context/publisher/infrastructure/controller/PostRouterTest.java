10b6854324485b8342a30c2e0dc76b46030abf96
john-weetaker <want4planch@gmail.com>
2019-01-22 07:57:43 +0100

    wip increment ut

--- src/test/java/com/evergarden/cms/context/publisher/infrastructure/controller/PostRouterTest.java
+++ src/test/java/com/evergarden/cms/context/publisher/infrastructure/controller/PostRouterTest.java
@@ -16,15 +16,16 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @ExtendWith(SpringExtension.class)
 @WebFluxTest
 class PostRouterTest {
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@MockBean
@@ -42,16 +43,16 @@ class PostRouterTest {
 
 	@Test
 	public void read() {
-		
+
 		UpdatedPost expectedPost = new UpdatedPost();
 		expectedPost.setAuthor("john");
 		expectedPost.setTitle("<h1>The book ever</h1>");
 		expectedPost.setBody("<b>This is a wonderfull book I ever read</b>");
 		expectedPost.setId(1L);
-		
+
 		BDDMockito.given(postRepository.fetchById(1L))
 			.willReturn(Mono.just(expectedPost));
-		
+
 		client.get()
 			.uri(env.getProperty("v1") + "/post/{id}", 1)
 			.accept(MediaType.APPLICATION_JSON)
@@ -64,21 +65,44 @@ class PostRouterTest {
 	@Test
 	public void show() {
 
-		UpdatedPost expectedPost = new UpdatedPost();
-		expectedPost.setAuthor("john");
-		expectedPost.setTitle("<h1>The book ever</h1>");
-		expectedPost.setBody("<b>This is a wonderfull book I ever read</b>");
-		expectedPost.setId(1L);
+		UpdatedPost expectedPost1 = new UpdatedPost();
+		expectedPost1.setAuthor("john");
+		expectedPost1.setTitle("<h1>The book ever</h1>");
+		expectedPost1.setBody("<b>This is a wonderfull book I ever read</b>");
+		expectedPost1.setId(1L);
 
-		BDDMockito.given(postRepository.fetchById(1L))
-				.willReturn(Mono.just(expectedPost));
+        UpdatedPost expectedPost2 = new UpdatedPost();
+        expectedPost2.setAuthor("john2");
+        expectedPost2.setTitle("<h1>The book ever 2</h1>");
+        expectedPost2.setBody("<b>This is a wonderfull book I ever read 2</b>");
+        expectedPost2.setId(2L);
+
+        UpdatedPost expectedPost3 = new UpdatedPost();
+        expectedPost3.setAuthor("john3");
+        expectedPost3.setTitle("<h1>The book ever 3</h1>");
+        expectedPost3.setBody("<b>This is a wonderfull book I ever read 3</b>");
+        expectedPost3.setId(3L);
+
+		BDDMockito.given(postRepository.fetchAll())
+				.willReturn(Flux.just(expectedPost1, expectedPost2, expectedPost3));
 
 		client.get()
-				.uri(env.getProperty("v1") + "/post/{id}", 1)
+				.uri(env.getProperty("v1") + "/post")
 				.accept(MediaType.APPLICATION_JSON)
 				.exchange()
 				.expectStatus().isOk()
-				.expectBody(UpdatedPost.class)
-				.isEqualTo(expectedPost);
+				.expectBodyList(UpdatedPost.class)
+                .hasSize(3)
+                .contains(expectedPost3)
+                .contains(expectedPost1);
 	}
+
+    @Test
+    public void delete() {
+        client.delete()
+                .uri(env.getProperty("v1s")+"/post")
+                .accept(MediaType.APPLICATION_JSON)
+                .body();
+
+    }
 }
