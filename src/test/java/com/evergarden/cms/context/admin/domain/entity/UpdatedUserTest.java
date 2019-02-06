c8763bc0cca40ab3ad750e119b3d9327d7541833
John-Weetaker <want4planch@gmail.com>
2019-02-06 18:13:55 +0100

    stam

--- src/test/java/com/evergarden/cms/context/admin/domain/entity/UpdatedUserTest.java
+++ src/test/java/com/evergarden/cms/context/admin/domain/entity/UpdatedUserTest.java
@@ -1,7 +1,64 @@
 package com.evergarden.cms.context.admin.domain.entity;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UpdatedUserTest {
-	
+    // Check assigned roles
+
+    @Test
+    void createInstanceAllParamConstructor() {
+        Collection<SimpleGrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        roles.add(new SimpleGrantedAuthority("ROLE_GUEST"));
+
+        UpdatedUser updatedUser = new UpdatedUser(
+            1,
+            true,
+            "violet@mail.com",
+            "Batou",
+            "Ranger",
+            "Batou",
+            "password",
+            roles
+        );
+
+        assertEquals(1, updatedUser.getId());
+        assertEquals("violet@mail.com", updatedUser.getEmail());
+        assertEquals("Batou", updatedUser.getFirstname());
+        assertEquals("Ranger", updatedUser.getLastname());
+        assertEquals("Batou", updatedUser.getPseudo());
+        assertEquals("password", updatedUser.getPassword());
+        assertTrue(updatedUser.isActivated());
+    }
+
+    @Test
+    void createInstanceWithDefaultConstructor() {
+        Collection<SimpleGrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+        UpdatedUser updatedUser = new UpdatedUser();
+
+        updatedUser.setEmail("violet@mail.com");
+        updatedUser.setFirstname("Batou");
+        updatedUser.setLastname("Ranger");
+        updatedUser.setPseudo("Batou");
+        updatedUser.setActivated(true);
+        updatedUser.setId(1);
+        updatedUser.setRoles(roles);
+
+        assertEquals(1, updatedUser.getId());
+        assertEquals("violet@mail.com", updatedUser.getEmail());
+        assertEquals("Batou", updatedUser.getFirstname());
+        assertEquals("Ranger", updatedUser.getLastname());
+        assertEquals("Batou", updatedUser.getPseudo());
+        assertEquals("password", updatedUser.getPassword());
+        assertTrue(updatedUser.isActivated());
+    }
+
 }
