44441eba84a033dea4db9213005dd5eade2e61cc
John Dormevil <jjdormevil@gmail.com>
2020-01-28 02:13:25 +0100

    format

--- src/integrationTest/java/com/evergarden/cms/context/publisher/application/service/crudpostservice/should_updated_previous_post.java
+++ src/integrationTest/java/com/evergarden/cms/context/publisher/application/service/crudpostservice/should_updated_previous_post.java
@@ -10,7 +10,7 @@ import reactor.test.StepVerifier;
 
 
 public class should_updated_previous_post extends IntegrationCmsApplicationTests {
-    
+
     @Autowired
     private CRUDPostService crudPostService;
 
